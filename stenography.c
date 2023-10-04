3e5bf66bb34ea921bf28bd4df2aa9d8cece05b42
jwSharp <jws146@pitt.edu>
2023-10-04 19:00:41 -0400

    Fix shift_bits(). Remove testing. Move documentation.
    - Enforce that shifts do not perform signed extention.
    - Remove testing found in reveal().
    - Add some function comments and move documentation to header file.

--- stenography.c
+++ stenography.c
@@ -14,7 +14,7 @@
 /*************** BMP File ***************/
 /****************************************/
 
-bmp_file create_bmp(char *filename)
+bmp_file open_bmp(const char *filename)
 {
     bmp_file bmp;
 
@@ -22,36 +22,36 @@ bmp_file create_bmp(char *filename)
     bmp.photo = fopen(filename, "r+");
     if (bmp.photo == NULL)
     {
-        fprintf(stderr, "Photo not successfully opened.");
+        fprintf(stderr, "Photo not successfully opened.\n");
         return bmp;
     }
 
     // Set bitmap file header values
-    check_read(fread(bmp.header.bitmap.id, sizeof(bmp.header.bitmap.id), 1, bmp.photo), 1);
+    checked_read(bmp.header.bitmap.id, sizeof(bmp.header.bitmap.id), 1, bmp.photo);
 
     if (strncmp(bmp.header.bitmap.id, "BM", 2))
     {
-        fprintf(stderr, "Incorrect file type.");
+        fprintf(stderr, "Incorrect file type.\n");
         bmp.photo = NULL;
         return bmp;
     }
-    check_read(fread(&bmp.header.bitmap.file_size, sizeof(bmp.header.bitmap.file_size), 1, bmp.photo), 1);
-    check_read(fread(&bmp.header.bitmap.reserved1, sizeof(bmp.header.bitmap.reserved1), 1, bmp.photo), 1);
-    check_read(fread(&bmp.header.bitmap.reserved2, sizeof(bmp.header.bitmap.reserved2), 1, bmp.photo), 1);
-    check_read(fread(&bmp.header.bitmap.offset, sizeof(bmp.header.bitmap.offset), 1, bmp.photo), 1);
+    checked_read(&bmp.header.bitmap.file_size, sizeof(bmp.header.bitmap.file_size), 1, bmp.photo);
+    checked_read(&bmp.header.bitmap.reserved1, sizeof(bmp.header.bitmap.reserved1), 1, bmp.photo);
+    checked_read(&bmp.header.bitmap.reserved2, sizeof(bmp.header.bitmap.reserved2), 1, bmp.photo);
+    checked_read(&bmp.header.bitmap.offset, sizeof(bmp.header.bitmap.offset), 1, bmp.photo);
 
     // Set DIB header values
-    check_read(fread(&bmp.header.dib.header_size, sizeof(bmp.header.dib.header_size), 1, bmp.photo), 1);
-    check_read(fread(&bmp.header.dib.width, sizeof(bmp.header.dib.width), 1, bmp.photo), 1);
-    check_read(fread(&bmp.header.dib.height, sizeof(bmp.header.dib.height), 1, bmp.photo), 1);
-    check_read(fread(&bmp.header.dib.planes, sizeof(bmp.header.dib.planes), 1, bmp.photo), 1);
-    check_read(fread(&bmp.header.dib.bpp, sizeof(bmp.header.dib.bpp), 1, bmp.photo), 1);
-    check_read(fread(&bmp.header.dib.scheme, sizeof(bmp.header.dib.scheme), 1, bmp.photo), 1);
-    check_read(fread(&bmp.header.dib.img_size, sizeof(bmp.header.dib.img_size), 1, bmp.photo), 1);
-    check_read(fread(&bmp.header.dib.hres, sizeof(bmp.header.dib.hres), 1, bmp.photo), 1);
-    check_read(fread(&bmp.header.dib.vres, sizeof(bmp.header.dib.vres), 1, bmp.photo), 1);
-    check_read(fread(&bmp.header.dib.num_colors, sizeof(bmp.header.dib.num_colors), 1, bmp.photo), 1);
-    check_read(fread(&bmp.header.dib.num_imp_colors, sizeof(bmp.header.dib.num_imp_colors), 1, bmp.photo), 1);
+    checked_read(&bmp.header.dib.header_size, sizeof(bmp.header.dib.header_size), 1, bmp.photo);
+    checked_read(&bmp.header.dib.width, sizeof(bmp.header.dib.width), 1, bmp.photo);
+    checked_read(&bmp.header.dib.height, sizeof(bmp.header.dib.height), 1, bmp.photo);
+    checked_read(&bmp.header.dib.planes, sizeof(bmp.header.dib.planes), 1, bmp.photo);
+    checked_read(&bmp.header.dib.bpp, sizeof(bmp.header.dib.bpp), 1, bmp.photo);
+    checked_read(&bmp.header.dib.scheme, sizeof(bmp.header.dib.scheme), 1, bmp.photo);
+    checked_read(&bmp.header.dib.img_size, sizeof(bmp.header.dib.img_size), 1, bmp.photo);
+    checked_read(&bmp.header.dib.hres, sizeof(bmp.header.dib.hres), 1, bmp.photo);
+    checked_read(&bmp.header.dib.vres, sizeof(bmp.header.dib.vres), 1, bmp.photo);
+    checked_read(&bmp.header.dib.num_colors, sizeof(bmp.header.dib.num_colors), 1, bmp.photo);
+    checked_read(&bmp.header.dib.num_imp_colors, sizeof(bmp.header.dib.num_imp_colors), 1, bmp.photo);
 
     return bmp;
 }
@@ -87,53 +87,36 @@ void reveal(bmp_file bmp)
     // Determine RGB Format
     if (bmp.header.dib.bpp != 24)
     {
-        fprintf(stderr, "Program does not handle alternate color densities. Image must be 24 bpp.");
-        fprintf(stdout, "Photo not revealed.");
+        fprintf(stderr, "Program does not handle alternate color densities. Image must be 24 bpp.\n");
+        fprintf(stdout, "Photo not revealed.\n");
         return;
     }
-    typedef struct
-    {
-        char r, g, b;
-    } rgb;
 
-    //
-    // Alter Colors
-    check_seek(fseek(bmp.photo, bmp.header.bitmap.offset, SEEK_SET), 0, bmp.header.bitmap.offset); // from beginning of file
+    // Create a color structure
     rgb color;
-    int padding = (sizeof(color) * bmp.header.dib.width) % 4; // Padding, align row to multiple of 4
 
+    // Update the photo's colors
+    checked_seek(bmp.photo, bmp.header.bitmap.offset, SEEK_SET); // seek to colors
+    int padding = (sizeof(color) * bmp.header.dib.width) % 4;    // Padding, align row to multiple of 4
     for (int h = 0; h < bmp.header.dib.height; h++)
     {
         for (int w = 0; w < bmp.header.dib.width; w++)
         {
-            check_read(fread(&color, sizeof(color.r), 3, bmp.photo), 3);
-            rgb before = color;
+            // Read the next color
+            checked_read(&color, sizeof(color.r), 3, bmp.photo);
 
-            // Swap Bits
+            // Swap bits of the color
             color.r = swap_bits(color.r);
             color.g = swap_bits(color.g);
             color.b = swap_bits(color.b);
 
-            // Update photo
-            check_seek(fseek(bmp.photo, -sizeof(color), SEEK_CUR), SEEK_CUR, -sizeof(color.r) * 3);
+            // Write to the photo
+            checked_seek(bmp.photo, -sizeof(color), SEEK_CUR);
             fwrite(&color, sizeof(color), 1, bmp.photo);
-
-            check_seek(fseek(bmp.photo, -sizeof(color), SEEK_CUR), SEEK_CUR, -sizeof(color.r) * 3);
-            check_read(fread(&color, sizeof(color.r), 3, bmp.photo), 3);
-            rgb after = color;
-            int is_same;
-            if (before.r == after.r)
-            {
-                is_same = 1;
-            }
-            else
-            {
-                is_same = 0;
-            }
-            fprintf(stderr, "%i", is_same);
         }
 
-        check_seek(fseek(bmp.photo, padding, SEEK_CUR), SEEK_CUR, padding); // End of row padding
+        // End of row padding
+        checked_seek(bmp.photo, padding, SEEK_CUR);
     }
 }
 
@@ -143,7 +126,7 @@ void reveal(bmp_file bmp)
 
 char swap_bits(char color)
 {
-    char msb, lsb, temp;
+    unsigned char msb, lsb, temp; // avoid signed ext
 
     // split color
     msb = 0xF0 & color;
@@ -152,13 +135,13 @@ char swap_bits(char color)
     // switch order
     temp = msb;
     msb = lsb << 4;
-    lsb = temp >> 4;
+    lsb = temp >> 4; // 1111MSB
     return lsb | msb;
 }
 
 char combine_bits(char color1, char color2)
 {
-    char msb, lsb;
+    unsigned char msb, lsb; // avoid signed extention
 
     // capture MSB of each color
     msb = 0xF0 & color1;
@@ -172,39 +155,35 @@ char combine_bits(char color1, char color2)
 /****************************************/
 /**************** Files *****************/
 /****************************************/
-
-/**
- * Checks for proper number of successfully read objects.
- */
-void check_read(size_t read, int goal)
+void checked_read(void *__restrict__ __ptr, size_t __size, size_t __nitems, FILE *__restrict__ __stream)
 {
-    if ((int)read != goal)
+    int read = fread(__ptr, __size, __nitems, __stream);
+    if ((int)read != __nitems)
     {
-        if ((int)read > 0)
+        if ((int)read == 0)
         {
-            fprintf(stderr, "Not all elements were read.");
+            fprintf(stderr, "No elements were read.\n");
         }
         else
         {
-            fprintf(stderr, "No elements were read.");
+            fprintf(stderr, "Not all elements were read.\n");
         }
     }
 }
 
-/**
- *
- */
-void check_seek(int failure, int origin, long offset)
+void checked_seek(FILE *file, long int offset, int whence)
 {
-    if (failure)
+    int temp_whence = whence;
+    int result = fseek(file, offset, whence);
+    if (result)
     {
         if (offset >= 0)
         {
-            fprintf(stderr, "Failed to seek from %i forward %ld", origin, offset);
+            fprintf(stderr, "Failed to seek from %i forward %ld", temp_whence, offset);
         }
         else
         {
-            fprintf(stderr, "Failed to seek from %i backwards %ld", origin, offset);
+            fprintf(stderr, "Failed to seek from %i backwards %ld", temp_whence, offset);
         }
     }
 }
