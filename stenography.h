13a9299d4f4f1ec8590b8e6340aac673bb78a72b
jwSharp <jws146@pitt.edu>
2023-10-04 19:01:33 -0400

    Move structures. Document most functions.

--- stenography.h
+++ stenography.h
@@ -8,51 +8,6 @@
 
 #include <stdio.h>
 
-/*****************/
-/*** BMP File ****/
-/*****************/
-/**
- * @brief Stores a bmp photo in the bmp_file structure.
- * @param filename The name of the bmp file.
- * @return Returns a bmp_file structure created from the photo.
- * @note bmp.photo set to NULL when filename is invalid.
- */
-bmp_file create_bmp(char *filename);
-
-/**
- * @brief Displays the BMP and DIB headers of a BMP file.
- * @details Takes a bmp photo and prints out the contents of the photo's header.
- * @param photo A bmp photo containing header information.
- */
-void display_header(bmp_file bmp);
-
-/**
- * Reveals an image hidden in the least significant bits of a given image.
- */
-void reveal(bmp_file bmp);
-
-/********************/
-/* Bit Manipulation */
-/********************/
-/**
- * @brief Swaps the most and least significant bits of a color.
- * @details Swaps the most significant bits with the
- * @param color A single color.
- * @return Returns a color with swapped bits.
- */
-char swap_bits(char color);
-
-/**
- * Use masks to combine the MSBs of two different photos.
- */
-char combine_bits(char color1, char color2);
-
-/*****************/
-/***** Files *****/
-/*****************/
-void check_read(size_t read, int goal);
-void check_seek(int failure, int origin, long offset);
-
 typedef struct
 {
     char id[2];
@@ -79,3 +34,73 @@ typedef struct
     bmp_header header;
     FILE *photo;
 } bmp_file;
+
+typedef struct
+{
+    char r, g, b;
+} rgb;
+
+/*****************/
+/*** BMP File ****/
+/*****************/
+/**
+ * @brief Stores a bmp photo in the bmp_file structure.
+ * @param filename The name of the bmp file.
+ * @return Returns a bmp_file structure created from the photo.
+ * @note bmp.photo set to NULL when filename is invalid.
+ */
+bmp_file open_bmp(const char *filename);
+
+/**
+ * @brief Displays the BMP and DIB headers of a BMP file.
+ * @details Takes a bmp photo and prints out the contents of the photo's header.
+ * @param photo A bmp photo containing header information.
+ */
+void display_header(bmp_file bmp);
+
+/**
+ * Reveals an image hidden in the least significant bits of a given image.
+ */
+void reveal(bmp_file bmp);
+
+void invert_image(const char *in_filename);
+
+void grayscale_image(const char *in_filename);
+
+void hflip_image(const char *in_filename);
+
+/********************/
+/* Bit Manipulation */
+/********************/
+/**
+ * @brief Swaps the most and least significant bits of a color.
+ * @details Swaps the most significant bits with the
+ * @param color A single color.
+ * @return Returns a color with swapped bits.
+ */
+char swap_bits(char color);
+
+/**
+ * Use masks to combine the MSBs of two different photos.
+ */
+char combine_bits(char color1, char color2);
+
+/*****************/
+/***** Files *****/
+/*****************/
+
+/**
+ * @brief Performs fseek and writes to stderr upon failure.
+ * @param file Pointer to the file stream.
+ * @param offset Number of bytes from position of `whence`.
+ * @param whence Position used as reference for offset: SEEK_SET, SEEK_CUR, or SEEK_END
+ */
+void checked_seek(FILE *file, long int offset, int whence);
+/**
+ * @brief Performs fread and writes to stderr upon failure.
+ * @param __ptr Pointer to where the read data should be copied.
+ * @param __size Size of the data.
+ * @param __nitems Number of items to be read.
+ * @param __stream File stream from which data is being read.
+ */
+void checked_read(void *__restrict__ __ptr, size_t __size, size_t __nitems, FILE *__restrict__ __stream);
