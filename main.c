db797b98dfa41eed19f54b8d412b8b9510ff17ca
jwSharp <jws146@pitt.edu>
2023-10-05 12:16:32 -0400

    Write basic terminal control flow for functions.

--- main.c
+++ main.c
@@ -13,30 +13,105 @@
  */
 int main(int argc, char **argv)
 {
-  // Open file
-  if (argc < 2)
+  printf("Welcome to image stenography.\n");
+  printf("Please select from the options below by typing the number of the operation you wish to perform:\n");
+
+  char filename[100], ftarget[100], fhidden[100];
+  bmp_file bmp, target, hidden;
+  int choice;
+
+  typedef int bool;
+  bool flag = 1;
+  while (flag)
   {
-    fprintf(stderr, "No file name given.");
-    return -1;
+    printf("\n1. Display Header\n");
+    printf("2. Reveal Photo\n");
+    printf("3. Hide Photo\n");
+    printf("Your Response:\t");
+
+    scanf("%d", &choice);
+    printf("\n");
+
+    switch (choice)
+    {
+    case 1:
+      // prompt for bmp file
+      printf("Enter the filename of the bmp file.\n");
+      scanf("%s", &filename);
+
+      // open the photo
+      bmp = open_bmp(filename);
+      if (bmp.photo == NULL)
+      {
+        fprintf(stderr, "BMP file %s could not be properly opened.\n", filename);
+        printf("Photo cannot be opened.\n");
+        continue;
+      }
+
+      display_header(bmp);
+
+      close_bmp(bmp);
+      break;
+
+    case 2:
+      // prompt for bmp file
+      printf("Enter the filename of the bmp file.\n");
+      scanf("%s", &filename);
+
+      // open the photo
+      bmp = open_bmp(filename);
+      if (bmp.photo == NULL)
+      {
+        fprintf(stderr, "BMP file %s could not be properly opened.\n", filename);
+        printf("Photo cannot be opened.\n");
+        continue;
+      }
+
+      // reveal the hidden photo
+      reveal(bmp);
+
+      close_bmp(bmp);
+      break;
+
+    case 3:
+      // prompt for bmp files
+      printf("Enter the filename of the bmp file that will be hidden.\n");
+      scanf("%s", &fhidden);
+      printf("Enter the filename of the target bmp file which will hold the hidden photo.\n");
+      scanf("%s", &ftarget);
+
+      // open the photos
+      target = open_bmp(ftarget);
+      hidden = open_bmp(fhidden);
+      if (target.photo == NULL || hidden.photo == NULL)
+      {
+        fprintf(stderr, "BMP files could not be properly opened.\n");
+        printf("One of the files cannot be opened.\n");
+        continue;
+      }
+
+      // hide the photo
+      hide(target, hidden);
+
+      close_bmp(target);
+      close_bmp(hidden);
+      break;
+
+    default:
+      printf("This is an invalid option.\n");
+      break;
+    }
+
+    // prompt for another operation
+    printf("Would you like to perform another operation?\n");
+    printf("Type 1 if you wish to do another operation or 0 if not.\n");
+    scanf("%d", &flag);
+    if (flag != 1)
+    {
+      flag = 0;
+    }
   }
-  bmp_file bmp = create_bmp(argv[1]);
 
-  // Check photo properly stored
-  if (bmp.photo == NULL)
-  {
-    fprintf(stderr, "BMP file could not be properly opened.");
-    printf("Photo cannot be opened.");
-    return -1;
-  }
-
-  // Display Headers
-  display_header(bmp.header);
-
-  // Reveal Hidden File
-  reveal(bmp);
-
-  // Close file
-  fclose(bmp.photo);
-
+  printf("\n\nThank you for using image stenography.");
   return 0;
 }
